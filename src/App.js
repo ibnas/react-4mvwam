f4481c8f8df914f5f17b08ff57935ca3825250ea
ibnas <ibnasir00@gmail.com>
2021-06-10 21:02:03 +0100

    1 file added

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Dragzone from './dragzone.js';
 import SVG from './svgContainer.js';
 import DraggableCircle from './draggableCircle';
 import DraggableRect from './draggableRect';
+import Text from './components/text';
 
 export default function App() {
   return (
@@ -15,7 +16,10 @@ export default function App() {
         <Line x1={0} y1={0} x2={300} y2={300} />
         <Line x1={300} y1={0} x2={0} y2={300} />
         <DraggableCircle x={100} y={200} r={30} />
-        <DraggableRect coord={{ x: 100, width: 100, y: 100, height: 200 }} />
+        <DraggableRect
+          coord={{ x: 100, width: 100, y: 100, height: 200 }}
+        />{' 
+        <Text />
       </Dragzone>
     </div>
   );
---
+++ src/components/text.js
@@ -0,0 +1,39 @@
+import React, { useState } from 'react';
+import Draggable from './../draggable';
+
+function Text(props) {
+  let [editing, setTediting] = useState(false);
+  let [text, setText] = useState('Text');
+  let [coor, setCoord] = useState([20, 35]);
+
+  let onDrg = dragState => {
+    setCoord([coor[0] + dragState.dx, coor[1] + dragState.dy]);
+    //positionChange(dragState);
+  };
+
+  let onClick = () => {
+    setTediting(!editing);
+  };
+  let r = !editing ? (
+    <text x={coor[0]} y={coor[1]} onDoubleClick={onClick}>
+      {text}
+    </text>
+  ) : (
+    <foreignObject
+      x={coor[0]}
+      y={coor[1]}
+      width={100}
+      height={100}
+      onDoubleClick={onClick}
+    >
+      <textarea>{text}</textarea>
+    </foreignObject>
+  );
+  return (
+    <Draggable context={props.context} onDrag={onDrg}>
+      {r}
+    </Draggable>
+  );
+}
+
+export default Text;
